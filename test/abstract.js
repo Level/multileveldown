8195deeb391c0463c5ed5ddbebecb23b5fa402b3
Julian Gruber <julian@juliangruber.com>
2015-04-27 13:33:12 +0200

    standard

--- test/abstract.js
+++ test/abstract.js
@@ -28,7 +28,7 @@ require('abstract-leveldown/abstract/iterator-test').setUp(factory, test, testCo
 require('abstract-leveldown/abstract/iterator-test').args(factory, test, testCommon)
 require('abstract-leveldown/abstract/iterator-test').sequence(factory, test, testCommon)
 
-//require('abstract-leveldown/abstract/leveldown-test').args(factory, test, testCommon)
+// require('abstract-leveldown/abstract/leveldown-test').args(factory, test, testCommon)
 require('abstract-leveldown/abstract/open-test').args(factory, test, testCommon)
 
 require('abstract-leveldown/abstract/put-get-del-test').setUp(factory, test, testCommon)
